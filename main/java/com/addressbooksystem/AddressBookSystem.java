ac8d9dd6f7c2548b3817df229f34f31f80bd8e08
prashantn/12 <prashantnangare0872@gmail.com>
2024-09-19 11:58:07 +0530

    Added: Welcome to address book system

---
+++ main/java/com/addressbooksystem/AddressBookSystem.java
@@ -0,0 +1,8 @@
+package com.addressbooksystem;
+import java.util.*;
+
+public class AddressBookSystem {
+    public static void main(String[] args) {
+        System.out.println("Welcome address book system");
+    }
+}
